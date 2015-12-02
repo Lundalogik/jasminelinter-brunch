32fce4e23e750c031f41c60195f640303f35a928
Johan Andersson <johan-spam@andersson.net>
2015-12-02 17:13:22 +0100

    Always lint check, but fail only if --production

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function JasmineLinter(config) {
   var cfg = config.plugins.jasminelinter;
   this.pattern = cfg.pattern || new RegExp( /^(test|spec)[\\/]/, 'i');
+  this.logMessage = config.env.indexOf( 'production' ) > -1 ?
+  	function(msg) { return msg; } :
+  	function(msg) { return 'warn: ' + msg; };
 }
 
 JasmineLinter.prototype.brunchPlugin = true;
-JasmineLinter.prototype.defaultEnv = 'production';
 
 JasmineLinter.prototype.lint = function(data, path, callback) {
   var self = this;
   if( this.pattern.test(path) && data.match(/(iit|ddescribe)\s*\(/)) {
-    callback('Found iit or ddescribe in ' + path);
+    callback(this.logMessage( 'Found iit or ddescribe in ' + path));
     return;
   }
   callback();
